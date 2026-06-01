76641cd745fa4bfec460a230cbcdf0c7ee0a98f5
tbodt <tbodt@tbodt.com>
2026-05-31 21:50:24 -0700

    o pana e ilo pi nanpa soweli lon lipu pi pini pali taso

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,25 +4,21 @@ import cloudflare from '@astrojs/cloudflare';
 import mdx from '@astrojs/mdx';
 import sitemap from "@astrojs/sitemap";
 
-const deploy =
-	import.meta.env.PROD ?
-		{ site: `https://tokipona.net/` }
-	:	{ site: "http://localhost/" };
-
 // https://astro.build/config
 export default defineConfig({
-	...deploy,
+	site: import.meta.env.CF_PAGES_URL ?? "http://localhost/",
+
 	// tenpo ni la ilo cloudflare pages li pana e lipu. nasin ona la, sitelen
 	// palisa pini o lon a nimi ilo lipu - sina lukin lukin e lipu kepeken ni ala
 	// la ilo li ante e nasin li pana e ni. taso jan li kepeken ilo Aso lon ilo
 	// ona la ni li kama ala la, sona jan li pakala. lawa ni li tawa ni: ilo Aso
 	// pi pali ilo o sama ilo pana lon cloudflare.
 	trailingSlash: 'always',
+
 	redirects: {
 		'/tp/Default.aspx': '/',
 	},
-	integrations: [mdx(), sitemap()],
-	adapter: cloudflare(),
+
 	fonts: [
 		{
 			provider: fontProviders.fontsource(),
@@ -32,4 +28,8 @@ export default defineConfig({
 			weights: [400, 700],
 		},
 	],
+
+	integrations: [mdx(), sitemap()],
+
+	adapter: cloudflare(),
 });
